a3351b2ee13d9e517477928467e6994c9693597b
Alexander Fortin <alexander.fortin@gmail.com>
2014-06-17 11:10:00 +0200

    Fix bug with apt key

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -3,19 +3,22 @@
 # https://forge.puppetlabs.com/puppetlabs/apt
 
 class tor::apt () {
+  $ensure = $::tor::ensure
 
   if $::tor::enable_apt_repo {
+
     include 'apt'
 
+    apt::key {'A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89':
+      key_server => 'keys.gnupg.net',
+    } ->
     apt::source { 'tor':
-      ensure            => $::tor::ensure,
+      ensure            => $ensure,
       location          => 'http://deb.torproject.org/torproject.org',
       release           => $::lsbdistcodename,
       repos             => 'main',
-      key               => 'A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89',
-      key_server        => 'keys.gnupg.net',
     } ->
-    package {'deb.torproject.org-keyring': ensure => $::tor::ensure}
-
+    package {'deb.torproject.org-keyring': ensure => $ensure}
   }
+
 }
